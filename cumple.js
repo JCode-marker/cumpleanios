f938317cdda71ae2dcc707e37d7fd21a37625479
JCode-marker <72767265+JCode-marker@users.noreply.github.com>
2021-03-05 13:12:41 -0500

    Add files via upload

---
+++ cumple.js
@@ -0,0 +1,38 @@
+// export
+    // variables
+    const d = document;
+    
+    function countdown(id, limitDate, finalMessage) {
+        
+        // variables
+        const $countdown = d.getElementById(id),
+            countdownDate = new Date(limitDate).getTime();
+
+        // setInterval
+        let countdownTempo = setInterval(() => {
+            let now = new Date().getTime(),
+                limitTime = countdownDate - now,
+                days = Math.floor(limitTime / (1000 * 60 * 60 * 24)),
+                hours = ("0" + Math.floor((limitTime % (1000 * 60 * 60 * 24)) / (1000 * 60 *60))).slice(-2),
+                minutes = ("0" + Math.floor((limitTime % (1000 * 60 * 60)) / (1000 * 60))).slice(-2),
+                seconds = ("0" + Math.floor((limitTime % (1000 * 60)) / (1000))).slice(-2);
+
+            $countdown.innerHTML = `
+                <h3>Faltan: ${days} dias ${hours} horas ${minutes} minutos ${seconds} segundos</h3>
+            `
+
+            // condicionales
+            if (limitTime < 0) {
+                clearInterval(countdownTempo);
+                $countdown.innerHTML = `<video src=${finalMessage} controls></video>`
+            }
+            
+        }, 1000);
+
+    }
+
+    countdown(
+        "countdown",
+        "Mar 05, 2021 18:00:00",
+        "cumpleaños-avengers.mp4"
+    )
